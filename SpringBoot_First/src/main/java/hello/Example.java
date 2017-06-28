543630714f4679261e3cba54ac67dd72595aacc9
shijin <shijin@peer1>
2017-06-28 23:19:00 +0800

    implement spring-cloud euraka server ribbon retry hystrix

--- SpringBoot_First/src/main/java/hello/Example.java
+++ SpringBoot_First/src/main/java/hello/Example.java
@@ -3,26 +3,26 @@ package hello;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-/**
- * 
- */
 
 /**
  * @author shijin
  *
  */
-@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication(scanBasePackages="hello.service")
 public class Example {
   private static final Logger LOGGER = LogManager.getLogger(Example.class);
 
   @Bean
+  @LoadBalanced
   public RestTemplate restTemplate(RestTemplateBuilder builder) {
     return builder.build();
   }
--- SpringBoot_First/src/main/java/hello/service/GreetingController.java
+++ SpringBoot_First/src/main/java/hello/service/GreetingController.java
@@ -8,13 +8,14 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 /**
  * @author shijin
@@ -28,6 +29,9 @@ public class GreetingController {
 
   @Autowired
   private RestTemplate restTemplate;
+
+  @Autowired
+  private DiscoveryClient client;
   
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -43,15 +47,33 @@ public class GreetingController {
     LOGGER.info("here is a INFO message");
     LOGGER.debug("here is a DEBUG message");
     LOGGER.trace("here is a TRACE message");
-
-    return "Hello World - Spring Boot!";
+    UriComponents uriComponents = UriComponentsBuilder.fromUriString("http://localhost:8080/user?name={name}").buildAndExpand("dodo").encode();
+    LOGGER.info(uriComponents);
+    
+    String netflixString = restTemplate.getForEntity("http://hello-netflix/netflix", String.class).getBody();
+    String ret = "Hello World - Spring Boot!" + netflixString!=null?"(" + netflixString + ")":"";
+    LOGGER.info("netflixString = " + ret);
+    return ret;
   }
 
   @RequestMapping("/quote")
   public Quote quote() {
     LOGGER.trace("remote call /quote.");
     Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-    
+    LOGGER.info("remote call:" + quote);
     return quote;
+  }
+  
+  @RequestMapping("/netflix")
+  public String netflix() {
+    
+    String hostName = client.getLocalServiceInstance().getHost();
+    String serviceId = client.getLocalServiceInstance().getServiceId();
+    int port = client.getLocalServiceInstance().getPort();
+    
+    LOGGER.info("/netflix, host:" + hostName + ", serviceId:" + serviceId + ", port:" + port);
+    
+    return "Netflix:" + port;
+    
   }
 }
